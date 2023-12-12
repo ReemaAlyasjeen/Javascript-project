fb08729cb02ca558360dee7f5dbfb9d23e950b39
ReemaAlyasjeen <149929557+ReemaAlyasjeen@users.noreply.github.com>
2023-12-12 12:11:22 +0200

    commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 let outputScreen = document.getElementById("output-screen");
 function display(num)
 {
-    outputScreen.value =+num;
+  
+    outputScreen.value = outputScreen.value+ num;
 }
 
     // clear 
@@ -9,9 +10,38 @@ function display(num)
     function Clear() {
       outputScreen.value = ''
     }
-    
-    // delete
-    
-    function del() {
-      outputScreen.value = outputScreen.value
+
+    // delete 
+    function  Del()
+    {
+      outputScreen.value=outputScreen.value.slice(0,-1)
     }
+    // Calculate
+    function Calculate()
+    {
+      outputScreen.value=eval(outputScreen.value)
+    }
+    
+    function fun()
+{
+  
+    outputScreen.value = outputScreen.value+ "(";
+}
+function func()
+{
+  
+    outputScreen.value = outputScreen.value+ ")";
+}
+ function sin()
+ {
+  outputScreen.value=Math.sin(outputScreen.value)
+ }
+ function cos()
+ {
+  outputScreen.value=Math.cos(outputScreen.value)
+ }
+ function tan()
+ {
+  outputScreen.value=Math.tan(outputScreen.value)
+ }
+   
